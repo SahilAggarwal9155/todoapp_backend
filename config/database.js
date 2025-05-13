3865a1fbedcf569b3815d76c1a182c93616fddaa
SahilAggarwal9155 <sahilaggarwal894@gmail.com>
2025-05-13 16:39:27 +0530

    Initalize the database and some files

---
+++ config/database.js
@@ -0,0 +1,8 @@
+const mongoose= require('mongoose');
+
+require('dotenv').config();
+const dbConnect = ()=>{
+  mongoose.connect(process.env.DATABASE_URL).then(()=>{console.log(`database id connected`)}).catch((err)=>{
+    console.log(`database is not connected successfully`,err);
+  })
+}
